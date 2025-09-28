32dfc27cb85d9bc60c6a42147ced163aba915a66
TYsonHe <1271477291@qq.com>
2025-09-28 11:55:07 +0800

    ✨ feat(coco):
    增加环境配置信息和匹配信息

---
+++ coco_get_matched.py
@@ -0,0 +1,119 @@
+from pycocotools.coco import COCO
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab  # matplotlib的一个模块，用于二维、三维图像绘制
+import os
+import json
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)  # 设置画布大小
+
+
+class CocoApi:
+    def __init__(self, dataDir, dataType, annFile):
+        self.dataDir = dataDir
+        self.dataType = dataType
+        self.annFile = annFile
+        self.coco = self.init_coco_api(self.annFile)
+
+    # 初始化COCO API
+
+    def init_coco_api(self, annFile):
+        coco = COCO(annFile)
+        return coco
+
+    def print_coco_cats(self):
+        self.catIds = self.coco.getCatIds()
+        print("Categories Ids: \n", self.catIds)
+        self.cats = self.coco.loadCats(self.catIds)
+        print("Categories: \n", self.cats)
+        self.cat_names = [cat['name'] for cat in self.cats]
+        # print("Categories Names: \n", self.cat_names)
+        self.super_cat_names = [cat['supercategory'] for cat in self.cats]
+        print("Super Categories Names: \n", self.super_cat_names)
+
+    def get_imgs_by_cat_names(self, cat_names):
+        imgIds = self.coco.getImgIds(
+            catIds=self.coco.getCatIds(catNms=cat_names))
+        print(f"cat_names{cat_names} total num of Image Ids: \n", len(imgIds))
+        imgs = self.coco.loadImgs(imgIds)
+        # print(f"cat_names{cat_names} Images: \n", imgs)
+
+        return imgIds, imgs
+
+    def get_imgs_by_img_ids(self, imgIds):
+        imgs = self.coco.loadImgs(imgIds)
+        # print(f"imgIds{imgIds} Images: \n", imgs)
+
+        return imgs
+
+    def get_anns_by_img_ids(self, imgIds):
+        annIds = self.coco.getAnnIds(imgIds=imgIds, iscrowd=None)
+        print(f"imgIds{imgIds} total num of Ann Ids: \n", len(annIds))
+        anns = self.coco.loadAnns(annIds)
+        # print(f"imgIds{imgIds} Annotations: \n", anns)
+
+        return annIds, anns
+
+    def show_imgs_by_img_ids(self, imgIds):
+        imgs = self.get_imgs_by_img_ids(imgIds)
+        for img in imgs:
+            I = io.imread('%s/%s/%s' %
+                          (self.dataDir, self.dataType, img['file_name']))
+            plt.axis('off')
+            plt.imshow(I)
+            plt.show()
+
+    def show_imgs_with_anns(self, imgIds):
+        imgs = self.get_imgs_by_img_ids(imgIds)
+        for img in imgs:
+            I = io.imread('%s/%s/%s' %
+                          (self.dataDir, self.dataType, img['file_name']))
+            plt.axis('off')
+            plt.imshow(I)
+            annIds = self.coco.getAnnIds(imgIds=img['id'], iscrowd=None)
+            anns = self.coco.loadAnns(annIds)
+            self.coco.showAnns(anns, draw_bbox=True)
+            plt.show()
+
+
+if __name__ == '__main__':
+    # 示例使用，获取100对包含指定类别名称的图像和注释，并保存
+    dataDir = './Datasets/MS_COCO'
+    dataType = 'val2014'
+    annFile = './Datasets/MS_COCO/annotations/instances_val2014.json'
+    coco_api = CocoApi(dataDir, dataType, annFile)
+    coco_api.print_coco_cats()
+    # cat_names = ['person', 'dog', 'cat'] 可以是一个list
+    cat_names = ['person']
+    imgIds, imgs = coco_api.get_imgs_by_cat_names(cat_names)
+
+    # 显示第一个图像和注释
+    coco_api.show_imgs_with_anns(imgIds[:1])
+
+    # 取前100个单独保存imgs和anns
+    imgIds = imgIds[:100]
+    imgs = imgs[:100]
+    annIds, anns = coco_api.get_anns_by_img_ids(imgIds)
+
+    test_100_imgpath = "./Datasets/MS_COCO/test_100_val2014_imgs"
+    test_100_annpath = "./Datasets/MS_COCO/test_100_val2014_anns"
+
+    # 保存100对imgs和anns
+    for i, img in enumerate(imgs):
+        I = io.imread('%s/%s/%s' %
+                      (dataDir, dataType, img['file_name']))
+        plt.axis('off')
+        plt.imshow(I)
+        if not os.path.exists(test_100_imgpath):
+            os.makedirs(test_100_imgpath)
+        plt.savefig(f"{test_100_imgpath}/{img['id']}.jpg")
+        plt.close()
+
+        annIds = coco_api.coco.getAnnIds(imgIds=img['id'], iscrowd=None)
+        anns = coco_api.coco.loadAnns(annIds)
+        coco_api.coco.showAnns(anns)
+        if not os.path.exists(test_100_annpath):
+            os.makedirs(test_100_annpath)
+
+        # ann保存为json格式,增加写入
+        with open(f"{test_100_annpath}/{img['id']}.json", 'w') as f:
+            json.dump(anns, f)
